e1aefaac17d543a3fcc2691d6276448784049233
finalxcode <finalxcode@gmail.com>
2025-05-17 20:12:43 +0800

    remove big log

--- module/Member/View/pc/register.blade.php
+++ module/Member/View/pc/register.blade.php
@@ -16,6 +16,8 @@
     {{\ModStart\ModStart::js('vendor/Member/entry/register.js')}}
     <script>
         $(function () {
+            var needCaptcha = {!! \Module\Member\Util\SecurityUtil::registerCaptchaProvider() ? 'false' : 'true' !!};
+            
             // 注册类型切换
             $('.register-type-item').click(function(){
                 $('.register-type-item').removeClass('active');
@@ -33,46 +35,53 @@
             });
             
             // 邮箱验证码
-            new window.api.commonVerify({
+            var emailVerifyOptions = {
                 generateServer: '{{$__msRoot}}register/email_verify',
                 selectorTarget: 'input[name=email]',
                 selectorGenerate: '[data-email-verify-generate]',
                 selectorCountdown: '[data-email-verify-countdown]',
                 selectorRegenerate: '[data-email-verify-regenerate]',
-                @if(!\Module\Member\Util\SecurityUtil::registerCaptchaProvider())
-                selectorCaptcha: 'input[name=captcha]',
-                selectorCaptchaImg:'[data-none]',
-                @endif
                 interval: 60,
-                validateOnBlur: false // 禁用失去焦点时验证
-            },window.api.dialog);
+                validateOnBlur: false,
+                validateOnChange: false,
+                validateOnInput: false,
+                validateOnKeyup: false,
+                validateOnGenerate: false,
+                validateOnSubmit: true
+            };
+            
+            if (needCaptcha) {
+                emailVerifyOptions.selectorCaptcha = 'input[name=captcha]';
+                emailVerifyOptions.selectorCaptchaImg = '[data-captcha-img]';
+            }
+            
+            new window.api.commonVerify(emailVerifyOptions);
 
             // 手机验证码
-            new window.api.commonVerify({
+            var phoneVerifyOptions = {
                 generateServer: '{{$__msRoot}}register/phone_verify',
                 selectorTarget: 'input[name=phone]',
                 selectorGenerate: '[data-phone-verify-generate]',
                 selectorCountdown: '[data-phone-verify-countdown]',
                 selectorRegenerate: '[data-phone-verify-regenerate]',
-                @if(!\Module\Member\Util\SecurityUtil::registerCaptchaProvider())
-                selectorCaptcha: 'input[name=captcha]',
-                selectorCaptchaImg:'[data-none]',
-                @endif
                 interval: 60,
-                validateOnBlur: false // 禁用失去焦点时验证
-            },window.api.dialog);
+                validateOnBlur: false,
+                validateOnChange: false,
+                validateOnInput: false,
+                validateOnKeyup: false,
+                validateOnGenerate: false,
+                validateOnSubmit: true
+            };
+            
+            if (needCaptcha) {
+                phoneVerifyOptions.selectorCaptcha = 'input[name=captcha]';
+                phoneVerifyOptions.selectorCaptchaImg = '[data-captcha-img]';
+            }
+            
+            new window.api.commonVerify(phoneVerifyOptions);
 
-            // 表单提交前验证
-            $('form[data-ajax-form]').on('submit', function(e) {
-                var form = $(this);
-                var captcha = form.find('input[name=captcha]');
-                if (captcha.length && !captcha.val()) {
-                    e.preventDefault();
-                    window.api.dialog.tip('请输入验证码');
-                    return false;
-                }
-                return true;
-            });
+            // 移除所有输入框的验证事件监听
+            $('input[name=email], input[name=phone], input[name=captcha]').off('blur change input keyup');
         });
     </script>
     {!! \ModStart\Core\Hook\ModStartHook::fireInView('MemberRegisterPageBodyAppend'); !!}
@@ -220,58 +229,12 @@
                 <!-- 大神入驻表单 -->
                 <form action="{{\ModStart\Core\Input\Request::currentPageUrl()}}" method="post" data-ajax-form class="register-form" data-type="expert" style="display:none;">
                     <input type="hidden" name="registerType" value="expert">
-                    <div class="line">
-                        <div class="field">
-                            <input type="text" class="form-lg" name="username" placeholder="用户名" />
-                        </div>
+                    
+                    <div class="expert-welcome">
+                        <h3>欢迎大神来到运动圈！</h3>
+                        <p>运动圈通过展会数万观众、自媒体、视频号、搜索引擎、B2B平台、网络媒体等大量引流，您将被更多人关注。如您某项运动成绩符合以下入库标准，请选择自己喜欢的内容并入驻相应大神库，这将有助于你成功入驻上手使用运动圈社区 ^_^</p>
                     </div>
-                    <div class="line">
-                        <div class="field">
-                            <input type="text" class="form-lg" name="realName" placeholder="真实姓名" />
-                        </div>
-                    </div>
-                    <div class="line">
-                        <div class="field">
-                            <input type="text" class="form-lg" name="expertise" placeholder="专业领域" />
-                        </div>
-                    </div>
-                    <div class="line">
-                        <div class="field">
-                            <textarea class="form-lg" name="introduction" placeholder="个人简介" rows="3"></textarea>
-                        </div>
-                    </div>
-                    @include('module::Member.View.pc.inc.registerCaptcha')
-                    @if(modstart_config('registerPhoneEnable'))
-                        <div class="line">
-                            <div class="field">
-                                <div class="row no-gutters">
-                                    <div class="col-7">
-                                        <input type="text" class="form-lg" name="phone" placeholder="输入手机" />
-                                    </div>
-                                    <div class="col-5">
-                                        <button class="btn btn-round btn-lg btn-block" type="button" data-phone-verify-generate>获取验证码</button>
-                                        <button class="btn btn-round btn-lg btn-block" type="button" data-phone-verify-countdown style="display:none;margin:0;"></button>
-                                        <button class="btn btn-round btn-lg btn-block" type="button" data-phone-verify-regenerate style="display:none;margin:0;">重新获取</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="line">
-                            <div class="field">
-                                <input type="text" class="form-lg" name="phoneVerify" placeholder="手机验证码" />
-                            </div>
-                        </div>
-                    @endif
-                    <div class="line">
-                        <div class="field">
-                            <input type="password" class="form-lg" name="password" placeholder="输入密码" />
-                        </div>
-                    </div>
-                    <div class="line">
-                        <div class="field">
-                            <input type="password" class="form-lg" name="passwordRepeat" placeholder="重复密码" />
-                        </div>
-                    </div>
+
                     <div class="line">
                         <div class="field">
                             <div class="sports-tags">
@@ -308,9 +271,140 @@
                             </div>
                         </div>
                     </div>
-                    @foreach(\Module\Member\Provider\RegisterProcessor\MemberRegisterProcessorProvider::listAll() as $provider)
-                        {!! $provider->render() !!}
-                    @endforeach
+
+                    <div class="line">
+                        <div class="field">
+                            <div class="expert-database">
+                                <div class="expert-database-title">选择您要录入的大神库</div>
+                                <div class="expert-database-list">
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="marathon_sub3">
+                                        <span>中国马拉松破三跑者库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="ultra_100k">
+                                        <span>中国百公里越野赛跑者库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="cycling_level3">
+                                        <span>达到国家三级运动员水平骑者库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="roller_level3">
+                                        <span>达到国家三级运动员标准轮滑大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="downhill_level3">
+                                        <span>达到国家三级运动员标准速降大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="mountaineering_level3">
+                                        <span>达到国家三级运动员标准登山大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="rock_climbing_level3">
+                                        <span>达到国家三级运动员标准攀岩大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="swimming_level3">
+                                        <span>达到国家三级运动员标准游泳大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="kayaking_level3">
+                                        <span>达到国家三级运动员标准皮划艇大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="diving_level3">
+                                        <span>达到国家三级运动员标准潜水大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="ice_climbing_level3">
+                                        <span>达到国家三级运动员标准攀冰大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="skiing_level3">
+                                        <span>达到国家三级运动员标准滑雪大神库</span>
+                                    </label>
+                                    <label class="database-item">
+                                        <input type="checkbox" name="expert_database[]" value="ice_skating_level3">
+                                        <span>达到国家三级运动员标准滑冰大神库</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="line">
+                        <div class="field">
+                            <div class="certificate-upload">
+                                <div class="certificate-title">上传您的证书</div>
+                                <div class="upload-area">
+                                    <input type="file" name="certificate" accept="image/*,.pdf" class="certificate-input" />
+                                    <div class="upload-tip">支持jpg、png、pdf格式，大小不超过10MB</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="line">
+                        <div class="field">
+                            <input type="text" class="form-lg" name="realName" placeholder="姓名（必填）" required />
+                        </div>
+                    </div>
+
+                    <div class="line">
+                        <div class="field">
+                            <input type="email" class="form-lg" name="email" placeholder="常用邮箱（必填）" required style="width: 100%;" />
+                        </div>
+                    </div>
+
+                    <div class="line">
+                        <div class="field">
+                            <input type="text" class="form-lg" name="location" placeholder="所在地区（必填）" required />
+                        </div>
+                    </div>
+
+                    <div class="line">
+                        <div class="field">
+                            <input type="text" class="form-lg" name="contactName" placeholder="联系信息（姓名须与证书一致）" required />
+                        </div>
+                    </div>
+
+                    @include('module::Member.View.pc.inc.registerCaptcha')
+                    @if(modstart_config('registerPhoneEnable'))
+                        <div class="line">
+                            <div class="field">
+                                <div class="row no-gutters">
+                                    <div class="col-7">
+                                        <input type="text" class="form-lg" name="phone" placeholder="输入手机" />
+                                    </div>
+                                    <div class="col-5">
+                                        <button class="btn btn-round btn-lg btn-block" type="button" data-phone-verify-generate>获取验证码</button>
+                                        <button class="btn btn-round btn-lg btn-block" type="button" data-phone-verify-countdown style="display:none;margin:0;"></button>
+                                        <button class="btn btn-round btn-lg btn-block" type="button" data-phone-verify-regenerate style="display:none;margin:0;">重新获取</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="line">
+                            <div class="field">
+                                <input type="text" class="form-lg" name="phoneVerify" placeholder="手机验证码" />
+                            </div>
+                        </div>
+                    @endif
+
+                    <div class="line">
+                        <div class="field">
+                            <input type="password" class="form-lg" name="password" placeholder="输入密码" required />
+                        </div>
+                    </div>
+
+                    <div class="line">
+                        <div class="field">
+                            <input type="password" class="form-lg" name="passwordRepeat" placeholder="重复密码" required />
+                        </div>
+                    </div>
+
                     <div class="line">
                         <div class="field">
                             <button type="submit" class="btn btn-round btn-primary btn-lg btn-block">提交注册</button>
@@ -531,6 +625,84 @@
         }
         .tag.active span {
             padding-right: 24px;
+        }
+
+        .expert-welcome {
+            margin-bottom: 30px;
+            padding: 20px;
+            background: #f8f9fa;
+            border-radius: 8px;
+        }
+        .expert-welcome h3 {
+            font-size: 18px;
+            color: #333;
+            margin-bottom: 10px;
+        }
+        .expert-welcome p {
+            font-size: 14px;
+            color: #666;
+            line-height: 1.6;
+        }
+        .expert-database {
+            padding: 15px;
+            background: #f8f9fa;
+            border-radius: 8px;
+        }
+        .expert-database-title {
+            font-size: 16px;
+            font-weight: bold;
+            margin-bottom: 15px;
+            color: #333;
+        }
+        .expert-database-list {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+        .database-item {
+            display: flex;
+            align-items: center;
+            padding: 10px;
+            background: #fff;
+            border: 1px solid #e0e0e0;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: all 0.3s;
+        }
+        .database-item:hover {
+            border-color: #007bff;
+        }
+        .database-item input[type="checkbox"] {
+            margin-right: 10px;
+        }
+        .certificate-upload {
+            padding: 15px;
+            background: #f8f9fa;
+            border-radius: 8px;
+        }
+        .certificate-title {
+            font-size: 16px;
+            font-weight: bold;
+            margin-bottom: 15px;
+            color: #333;
+        }
+        .upload-area {
+            border: 2px dashed #ddd;
+            padding: 20px;
+            text-align: center;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        .upload-area:hover {
+            border-color: #007bff;
+        }
+        .certificate-input {
+            display: none;
+        }
+        .upload-tip {
+            font-size: 12px;
+            color: #666;
+            margin-top: 10px;
         }
     </style>
 @endsection
